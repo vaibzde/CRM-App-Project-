a85e2d170885da6caa0d04921dd34a57e7caaf2f
Vaibhav Dewangan <vaibhavdewanganaol@gmail.com>
2022-12-29 11:36:49 +0530

    updated routes for findby and update control

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,14 @@ const userController = require("../controllers/user.controller")
 module.exports = function(app){
     app.get(`/crm/api/users/`, [authJwt.verifyToken, authJwt.isAdmin], 
     userController.findAll)
+
+    app.get(`/crm/api/users/:userId`,
+    [authJwt.verifyToken, authJwt.isAdmin],
+    userController.findById)
+
+    app.put(`/crm/api/users/:userId`, 
+    [authJwt.verifyToken, authJwt.isAdmin],
+    userController.update)
 }
 
 //here [authJwt is the middleware]
