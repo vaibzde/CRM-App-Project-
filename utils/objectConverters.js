ab15f1b16743023067b70bc0959b9e7399189a15
Vaibhav Dewangan <vaibhavdewanganaol@gmail.com>
2022-12-29 08:03:03 +0530

    Create objectConverters.js

---
+++ utils/objectConverters.js
@@ -0,0 +1,13 @@
+exports.userResponse = (users) => {
+    let userResult = []
+    users.forEach(user => {
+        userResult.push({
+            name : user.name,
+            userId : user.userId,
+            email : user.email,
+            userType : user.userType,
+            userStatus : user.userStatus
+        })
+    })
+    return userResult
+}
