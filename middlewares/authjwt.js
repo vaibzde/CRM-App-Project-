0644662629fcaa687f77a9adfb025885d249e369
Vaibhav Dewangan <vaibhavdewanganaol@gmail.com>
2022-12-29 11:37:28 +0530

    developed isAdmin middleware

--- middlewares/authjwt.js
+++ middlewares/authjwt.js
@@ -1,25 +1,48 @@
 const jwt = require("jsonwebtoken")
 const authConfig = require("../configs/auth.config")
+const constants = require("../utils/constants")
+const { userTypes } = require("../utils/constants")
+const User = require("../models/user.model")
 
-verifyToken = (req, res, next) => {
+const verifyToken = (req, res, next) => {
     let token = req.header[`x-access-token`]
 
     if(!token) {
         return res.status(403).send({
-            message : "No taken provided!"
+            message : "No token provided!"
         })
     }
 
-    jwt.verify(token, authConfig.secret, (err, decode) => {
-        if(err, decode){
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if(err, decoded){
             return res.status(401).send(
                 {
                     message : "Unauthorized!"
                 }
             )
         }
-        req.body.userId = decode.id
+        req.body.userId = decoded.id
         next()
     })
 }
 //middleware exist between routers and controllers..
+
+const isAdmin = async(req, res, next) => {
+    const user = await User.findOne({
+        userId: req.body.userId
+    })
+    if(user && userTypes == constants.userTypes.admin){
+        next();
+    }else {
+        res.status(403).send({
+            message: "Require Admin Role!"
+        })
+        return
+    }
+}
+
+module.exports = {
+    verifyToken : verifyToken,
+    isAdmin : isAdmin
+}
+
