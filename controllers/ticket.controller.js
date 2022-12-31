3149a838819c95ff1a9498e4df5d60b8d8776a62
Vaibhav Dewangan <vaibhavdewanganaol@gmail.com>
2022-12-31 08:41:01 +0530

    Adding the update feature for the tickets in CRM application

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -35,12 +35,52 @@ exports.createTicket = async (res,req) => {
             res.status(201).send(objectConvertor.ticketResponse(ticket))
         }
     } catch (err) {
-        console.log("Some error happened while creating ticket ")
+        console.log("Some error happened while creating ticket ", err.message )
+        res.status(500).send({
+            message: "Some internal server error"
+        })
     }
 }
 
+const canUpdate = (user, ticket) => {
+    return user.userId == ticket.reporter ||
+            user.userId == ticket.assignee ||
+            user.userType == constants.userTypes.admin
+}
 
-exports.updateTicket  = async (res,req) => {}
+exports.updateTicket  = async (res,req) => {
+    const ticket = await Ticket.findOne({_id : req.param.id})
+
+    const savedUser = await User.findOne({
+        userId: req.body.userId 
+    })
+
+    if(canUpdate(savedUser, ticket)){
+            ticket.title = req.body.title != undefined
+                                    ? req.body.title
+                                    : ticket.title
+            ticket.description = req.body.description != undefined
+                                    ? req.body.description
+                                    : ticket.description
+            ticket.ticketPriority = req.body.ticketPriority != undefined
+                                    ? req.body.ticketPriority
+                                    : ticket.ticketPriority
+            ticket.status = req.body.status != undefined
+                                    ? req.body.status
+                                    : ticket.status
+            ticket.assignee = req.body.assignee != undefined
+                                ? req.body.assignee
+                                : ticket.assignee
+            await ticket.save()
+            res.status(200).send(objectConvertor.ticketResponse(ticket))
+    } else {
+        console.log("Ticket update was attempted by someone without access to the ticket")
+        res.status(401).send({
+            message : "Ticket can only be created by the customer who created"
+        })
+    }
+
+}
 
 
 exports.getAllTickets = async (res, req) => {}
--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -5,5 +5,10 @@ const authJwt = require("../middlewares/authjwt")
 module.exports = function(app) {
     app.post("/crm/api/tickets/", 
     [authJwt.verifyToken], 
-    ticketController.createTicket)
+    ticketController.createTicket),
+    
+    app.put("/crm/api/tickets/:id",
+    [authJwt.verifyToken],
+    ticketController.updateTicket)
+
 }
